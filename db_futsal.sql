41ee4a48e2aeb3551e036928b4c6d569cf33f07b
ericko-cyber <ulumuddini585@gmail.com>
2023-11-07 21:49:31 +0700

    Update db_futsal.sql
    penambahan database

--- db_futsal.sql
+++ db_futsal.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2023 at 05:24 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Nov 07, 2023 at 03:47 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,7 +41,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id_user`, `username`, `password`, `nama`, `phone`, `email`) VALUES
-(1, 'admin@admin', 'admin', 'Admin Rojak', '08902932', 'admin@admin');
+(1, 'admin@admin', 'admin', 'Admin Rojak', '0895', 'admin@admin');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,24 @@ CREATE TABLE `bayar` (
 --
 
 INSERT INTO `bayar` (`idbayar`, `idsewa`, `bukti`, `tgl_upload`, `konfirmasi`) VALUES
-(55, 123, '64522a4de1d9a.png', '2023-05-03', 'Terkonfirmasi');
+(55, 123, '64522a4de1d9a.png', '2023-05-03', 'Terkonfirmasi'),
+(56, 125, '652d56583731e.jpg', '2023-10-16', 'Sudah Bayar'),
+(57, 126, '652d5e0c52e99.png', '2023-10-16', 'Terkonfirmasi'),
+(58, 127, '652e32c2dab35.jpg', '2023-10-17', 'Terkonfirmasi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bayarmkn`
+--
+
+CREATE TABLE `bayarmkn` (
+  `idbayarmkn` int(11) NOT NULL,
+  `idpesan` int(11) NOT NULL,
+  `bukti` text NOT NULL,
+  `tgl_upload` date NOT NULL,
+  `konfirmasi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -86,7 +103,46 @@ INSERT INTO `lapangan` (`idlap`, `nm`, `ket`, `harga`, `foto`) VALUES
 (23, 'Dewa', 'ini lapangan Dewa', 30000, 'footbal.jpg'),
 (24, 'Emas', 'Ini Lapangan Emas', 10000, 'badmintoon.jpg'),
 (25, 'Silver', 'Ini Lapangan Silver', 50000, 'basket.jpg'),
-(26, 'Golf', 'Ini Lapangan Golf4', 40000, 'futsal.jpg');
+(26, 'Golf', 'Ini Lapangan Golf4', 40000, 'futsal.jpg'),
+(28, 'coba', '', 10000, '6545b1010840b.png'),
+(30, 'coba', '', 10000, '6545b720cad4f.png'),
+(31, 'coba', '', 10000, '6545b81d5b6bf.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `makanan`
+--
+
+CREATE TABLE `makanan` (
+  `idmakanan` int(11) NOT NULL,
+  `nm` varchar(35) NOT NULL,
+  `stok` text NOT NULL,
+  `harga` int(11) NOT NULL,
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `makanan`
+--
+
+INSERT INTO `makanan` (`idmakanan`, `nm`, `stok`, `harga`, `foto`) VALUES
+(4, 'Coba1', '10', 10000, '6545cd9d958ea.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `idpesan` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `idmakanan` int(11) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `harga` int(11) NOT NULL,
+  `tot` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -111,7 +167,10 @@ CREATE TABLE `sewa` (
 --
 
 INSERT INTO `sewa` (`idsewa`, `iduser`, `idlap`, `tgl_pesan`, `lama`, `jmulai`, `jhabis`, `harga`, `tot`) VALUES
-(123, 98, 23, '2023-05-03', 2, '2023-05-03 16:23:00', '2023-05-03 18:23:00', 30000, 60000);
+(126, 98, 23, '2023-10-16', 2, '2023-10-16 22:59:00', '2023-10-17 00:59:00', 30000, 60000),
+(127, 98, 24, '2023-10-17', 2, '2023-10-17 14:06:00', '2023-10-17 16:06:00', 10000, 20000),
+(128, 98, 23, '2023-10-26', 0, '2023-10-26 01:00:00', '2023-10-26 01:00:00', 30000, 30000),
+(129, 98, 24, '2023-10-26', 23, '0000-00-00 00:00:00', '1970-01-02 00:00:00', 10000, 10000);
 
 -- --------------------------------------------------------
 
@@ -127,15 +186,17 @@ CREATE TABLE `user` (
   `jenis_kelamin` varchar(10) NOT NULL,
   `nama_lengkap` varchar(60) NOT NULL,
   `alamat` text NOT NULL,
-  `foto` text NOT NULL
+  `foto` text NOT NULL,
+  `reset_token_hash` varchar(64) DEFAULT NULL,
+  `reset_token_expires_at` datetime DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `email`, `password`, `hp`, `jenis_kelamin`, `nama_lengkap`, `alamat`, `foto`) VALUES
-(98, 'risky12@gmail.com', '123', '08972423', 'Laki-laki', 'Rizky', 'Bekasi', '645229918b946.jpg');
+INSERT INTO `user` (`id_user`, `email`, `password`, `hp`, `jenis_kelamin`, `nama_lengkap`, `alamat`, `foto`, `reset_token_hash`, `reset_token_expires_at`) VALUES
+(98, 'ihyau855@gmail.com', '$2y$10$uLIpLKwYFtUSukBHYof3we.35', '08972423', 'Laki-laki', 'Rizky', 'Bekasi', '645229918b946.jpg', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -154,10 +215,28 @@ ALTER TABLE `bayar`
   ADD PRIMARY KEY (`idbayar`);
 
 --
+-- Indexes for table `bayarmkn`
+--
+ALTER TABLE `bayarmkn`
+  ADD PRIMARY KEY (`idbayarmkn`);
+
+--
 -- Indexes for table `lapangan`
 --
 ALTER TABLE `lapangan`
   ADD PRIMARY KEY (`idlap`);
+
+--
+-- Indexes for table `makanan`
+--
+ALTER TABLE `makanan`
+  ADD PRIMARY KEY (`idmakanan`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`idpesan`);
 
 --
 -- Indexes for table `sewa`
@@ -169,7 +248,8 @@ ALTER TABLE `sewa`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `reset_token_hash` (`reset_token_hash`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -179,31 +259,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `bayar`
 --
 ALTER TABLE `bayar`
-  MODIFY `idbayar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `idbayar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `bayarmkn`
+--
+ALTER TABLE `bayarmkn`
+  MODIFY `idbayarmkn` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `lapangan`
 --
 ALTER TABLE `lapangan`
-  MODIFY `idlap` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `idlap` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `makanan`
+--
+ALTER TABLE `makanan`
+  MODIFY `idmakanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `idpesan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `sewa`
 --
 ALTER TABLE `sewa`
-  MODIFY `idsewa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `idsewa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
