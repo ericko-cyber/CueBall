97120f340ac146509a250e77877428ea82c59ac0
nabilahhp <putrinabilah2003@gmail.com>
2023-11-24 11:52:33 +0700

    Update indexuser.php

--- indexuser.php
+++ indexuser.php
@@ -239,7 +239,7 @@ $select_products = mysqli_query($conn, "SELECT * FROM `makanan`");
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Basecamp <span>Billiard</span></h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h2>Your favorite Billiards place in town!</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
             <a href="#about" class="btn-get-started scrollto">Get Started</a>
             <a href="" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
