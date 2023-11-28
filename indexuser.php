bfae6e6823e1b7bd4494f348910a16210d55c766
dindanausa <dinda.nausa.888@gmail.com>
2023-11-28 16:06:12 +0700

    update responsive

--- indexuser.php
+++ indexuser.php
@@ -365,7 +365,7 @@ $select_products = mysqli_query($conn, "SELECT * FROM `makanan`");
 
         <div class="row">
 
-        <div class="col-md-6 " data-aos="fade-up" data-aos-delay="100">
+        <div class="col-md-6 " data-aos="fade-up"  data-aos-delay="100">
             <div class="box">
               <h3>Small Table</h3>
               <h4>12k<span>/hour</span></h4>
